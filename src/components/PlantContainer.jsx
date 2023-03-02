4869849704e3796172c2a614fa29fb4af5d197e0
Josen Romero <romerojosemanuel@hotmail.com>
2023-03-01 20:04:55 -0600

    add SocialMedia component

--- src/components/PlantContainer.jsx
+++ src/components/PlantContainer.jsx
@@ -1,12 +1,14 @@
 import { useSpriteAnimation } from '../hooks/useSpriteAnimation'
+import SocialMedia from './SocialMedia'
 
 const PlantContainer = () => {
 
   const { viewer, scene } = useSpriteAnimation();
 
   return (
-    <section ref={scene} className='!h-full !w-full'>
-      <div ref={viewer} className='viewer'></div>
+    <section ref={scene} className="!h-full !w-10/12 !mx-auto flex flex-col gap-5 items-center">
+      <div ref={viewer} className="viewer"></div>
+      <SocialMedia />
     </section>
   )
 
