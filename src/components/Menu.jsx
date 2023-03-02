db016c3fee145f3a6249761e874ce5748c573e58
Josen Romero <romerojosemanuel@hotmail.com>
2023-03-02 17:59:55 -0600

    add background image

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -2,9 +2,9 @@
 const Menu = () => {
 
   return (
-    <nav className="border-b border-zinc-500 p-4">
+    <nav className="bg-lime-900 opacity-50 p-4">
       <div className="md:w-3/4 mx-auto flex items-center md:justify-between gap-x-3">
-        <a className="text-lime-400 font-semibold text-2xl hover:text-lime-600" href="/">meica05</a>
+        <a className="text-lime-400 font-semibold text-2xl hover:text-lime-200" href="/">meica05</a>
       </div>
     </nav>
   )
