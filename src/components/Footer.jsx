80bc6b2189b1202e68ce9fb17c2116d3caedb50d
Josen Romero <romerojosemanuel@hotmail.com>
2023-03-07 19:29:59 -0600

    update Footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,8 +3,8 @@ import LinkTag from './LinkTag'
 const Footer = () => {
 
   return (
-    <footer className="text-white text-center text-xl p-4">
-      <p>Built by <LinkTag to={"https://github.com/JosenRomero"} text={"José Romero"} /></p>
+    <footer className="text-white text-center text-base p-4">
+      <p>Built by <LinkTag to={"https://github.com/JosenRomero"} text={"José Romero"} />, background by <LinkTag to={"https://www.svgbackgrounds.com/"} text={"SVGBackgrounds.com"} /> </p>
     </footer>
   )
 
