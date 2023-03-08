a5543ace230427b8efcb9d3c42dacefd84b14f2a
Josen Romero <romerojosemanuel@hotmail.com>
2023-03-08 17:34:04 -0600

    update PlantContainer component

--- src/components/PlantContainer.jsx
+++ src/components/PlantContainer.jsx
@@ -6,6 +6,7 @@ const PlantContainer = () => {
   return (
     <section className="!w-10/12 !mx-auto flex flex-col gap-9 items-center">
       <img className="rounded-full border-white border-2 w-48 h-48" src={meica_img} alt={"meica"} />
+      <p className="text-white text-center text-3xl font-bold">Meica05</p>
       <SocialMedia />
     </section>
   )
