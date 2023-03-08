213abe92e5b692e5076545f747fa3aba69b09550
Josen Romero <romerojosemanuel@hotmail.com>
2023-03-07 18:39:06 -0600

    delete gsap

--- src/components/PlantContainer.jsx
+++ src/components/PlantContainer.jsx
@@ -1,13 +1,10 @@
-import { useSpriteAnimation } from '../hooks/useSpriteAnimation'
 import SocialMedia from './SocialMedia'
 
 const PlantContainer = () => {
 
-  const { viewer, scene } = useSpriteAnimation();
-
   return (
-    <section ref={scene} className="!h-full !w-10/12 !mx-auto flex flex-col gap-5 items-center">
-      <div ref={viewer} className="viewer"></div>
+    <section className="!w-10/12 !mx-auto flex flex-col gap-5 items-center">
+      <div></div>
       <SocialMedia />
     </section>
   )
