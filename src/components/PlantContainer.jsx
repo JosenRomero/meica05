6c059c7c8df636f656bff3632667cad5f6b12a46
Josen Romero <romerojosemanuel@hotmail.com>
2023-03-07 19:30:54 -0600

    add image principal

--- src/components/PlantContainer.jsx
+++ src/components/PlantContainer.jsx
@@ -1,10 +1,11 @@
+import meica_img from '../assets/meica_img.jpg'
 import SocialMedia from './SocialMedia'
 
 const PlantContainer = () => {
 
   return (
-    <section className="!w-10/12 !mx-auto flex flex-col gap-5 items-center">
-      <div></div>
+    <section className="!w-10/12 !mx-auto flex flex-col gap-9 items-center">
+      <img className="rounded-full border-white border-2 w-48 h-48" src={meica_img} alt={"meica"} />
       <SocialMedia />
     </section>
   )
