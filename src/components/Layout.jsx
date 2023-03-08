4150d7351bc258476a0b48c4b4b2a681b2c098ab
Josen Romero <romerojosemanuel@hotmail.com>
2023-03-07 18:48:11 -0600

    update Menu component

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -4,11 +4,11 @@ import Footer from './Footer'
 const Layout = ({children}) => {
   
   return (
-    <>
+    <div className="min-h-screen grid grid-cols-1 place-content-between">
       <Menu />
       {children}
       <Footer />
-    </>
+    </div>
   )
 
 }
--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -3,14 +3,11 @@ import ItemMenu from './ItemMenu'
 const Menu = () => {
 
   return (
-    <nav className="p-2">
-      <div className="md:w-3/4 mx-auto flex items-center md:justify-between gap-x-3">
-        <ItemMenu to={"/"} text={"meica05"} className={"font-semibold text-2xl"} /> 
-        <div className="flex gap-x-3">
-          <ItemMenu to={"/"} text={"Inicio"} />
-          <ItemMenu to={"about"} text={"Bio"} />
-          <ItemMenu  to={"hashtags"} text={"Hashtags"} />
-        </div>
+    <nav className="p-4">
+      <div className="md:w-3/4 mx-auto flex justify-end gap-x-3">
+        <ItemMenu to={"/"} text={"Inicio"} />
+        <ItemMenu to={"about"} text={"Bio"} />
+        <ItemMenu  to={"hashtags"} text={"Hashtags"} />
       </div>
     </nav>
   )
