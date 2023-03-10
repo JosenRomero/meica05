7b7f0ec1a5372900728c6b559ae7e935e43c4088
Josen Romero <romerojosemanuel@hotmail.com>
2023-03-10 17:42:24 -0600

    add typewriter effect

--- src/components/PlantContainer.jsx
+++ src/components/PlantContainer.jsx
@@ -1,4 +1,5 @@
 import meica_img from '../assets/meica_img.jpg'
+import { Typewriter } from 'react-simple-typewriter'
 import SocialMedia from './SocialMedia'
 
 const PlantContainer = () => {
@@ -6,7 +7,9 @@ const PlantContainer = () => {
   return (
     <section className="!w-10/12 !mx-auto flex flex-col gap-9 items-center">
       <img className="rounded-full border-white border-2 w-48 h-48" src={meica_img} alt={"meica"} />
-      <p className="text-white text-center text-3xl font-bold">Meica05</p>
+      <p className="text-white text-center text-3xl font-bold h-[40px]">
+        <Typewriter words={["Meica05", "vtuber", "plantita"]} loop={false} cursor={true} />
+      </p>
       <SocialMedia />
     </section>
   )
