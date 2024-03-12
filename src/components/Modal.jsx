b0ad9e257387ea063ef4a69253e956bd1b0f8302
Jose <romerojosemanuel@hotmail.com>
2024-03-11 18:15:26 -0600

    add animation to Modal component

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,3 +1,4 @@
+import { Zoom } from 'react-awesome-reveal'
 
 const Modal = ({ children, isOpen, closeModal, title }) => {
 
@@ -7,13 +8,15 @@ const Modal = ({ children, isOpen, closeModal, title }) => {
 
   return (
     <div className={`bg-black/75 fixed top-0 left-0 w-screen h-screen z-10 justify-center items-center ${isOpen ? 'grid' : 'hidden'}`} onClick={closeModal}>
-      <div className="p-5 bg-white rounded-xl" onClick={handleModalDialogClick}>
-        <div className="flex">
-          <h2 className="text-center text-slate-500 grow">{ title }</h2>
-          <button className="flex-none text-black" onClick={closeModal}>X</button>
+      <Zoom>
+        <div className="p-5 bg-white rounded-xl" onClick={handleModalDialogClick}>
+          <div className="flex">
+            <h2 className="text-center text-slate-500 grow">{ title }</h2>
+            <button className="flex-none text-black" onClick={closeModal}>X</button>
+          </div>
+          { children }
         </div>
-        { children }
-      </div>
+      </Zoom>
     </div>
   )
 
--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,4 +1,4 @@
-import { AttentionSeeker } from 'react-awesome-reveal'
+import { Slide } from 'react-awesome-reveal'
 import { infoBasic, likes, dislikes } from '../data.js'
 import ItemBox from '../components/ItemBox'
 
@@ -6,7 +6,7 @@ const AboutPage = () => {
 
   return (
     <div className="w-4/5 h-full text-zinc-500 mx-auto mt-20 leading-relaxed">
-      <AttentionSeeker effect='pulse'>
+      <Slide direction='down'>
         <div className="mb-5 flex gap-9 justify-center flex-wrap">
           <ItemBox items={infoBasic} />
           <div className="flex gap-9 justify-center flex-wrap">
@@ -14,7 +14,7 @@ const AboutPage = () => {
             <ItemBox items={dislikes} title="Disgustos" />
           </div>
         </div>
-      </AttentionSeeker>
+      </Slide>
     </div>
   )
 
