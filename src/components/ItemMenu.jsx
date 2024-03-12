13f367c6fe9e8f1efb69bde044335ea5a0a092dc
Jose <romerojosemanuel@hotmail.com>
2024-03-11 18:58:35 -0600

    change menu style

--- src/components/ItemMenu.jsx
+++ src/components/ItemMenu.jsx
@@ -1,11 +1,19 @@
-import { Link } from 'react-router-dom'
+import { NavLink } from "react-router-dom"
 
-const ItemMenu = ({ to, text, className = ""}) => {
-
+const ItemMenu = ({ to, text }) => {
   return (
-    <Link className={`text-lime-700 bg-white px-4 py-2 rounded-xl hover:opacity-75 hover:text-lime-700 ${className}`} to={to}>{text}</Link>
+    <NavLink 
+      className={({ isActive }) => 
+        [
+          isActive ? "bg-stone-500 text-stone-100" : "bg-white text-slate-800",
+          "font-medium text-sm px-5 py-2.5 rounded-xl hover:bg-stone-500 hover:text-stone-100"
+        ].join(" ")
+      } 
+      to={to}
+    >
+      {text}
+    </NavLink>
   )
-
 }
 
 export default ItemMenu
