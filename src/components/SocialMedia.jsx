fa276571da1990f8f8e69302a01854f90b8a2f62
Jose <romerojosemanuel@hotmail.com>
2024-03-11 18:17:26 -0600

    add hover in SocialMedia component

--- src/components/SocialMedia.jsx
+++ src/components/SocialMedia.jsx
@@ -5,11 +5,11 @@ const SocialMedia = () => {
 
   return (
     <div className="flex gap-5 items-center [&>a]:bg-white [&>a]:p-2.5 [&>a]:rounded-full">
-      <a className="hover:opacity-75" href="https://www.youtube.com/@meica05" target="_blank"><FaYoutube size={32} color={"#65a30d"} /></a>
-      <a className="hover:opacity-75" href="https://www.twitch.tv/meica05" target="_blank"><FaTwitch size={32} color={"#65a30d"} /></a>
-      <a className="hover:opacity-75" href="https://www.tiktok.com/@meica05offcial" target="_blank"><FaTiktok size={32} color={"#65a30d"} /></a>
-      <a className="hover:opacity-75" href="https://twitter.com/meica05" target="_blank"><FaTwitter size={32} color={"#65a30d"} /></a>
-      <a className="hover:opacity-75" href="https://www.instagram.com/meica05/" target="_blank"><FaInstagram size={32} color={"#65a30d"} /></a>
+      <a className="inline-block hover:scale-125 hover:opacity-75 transition" href="https://www.youtube.com/@meica05" target="_blank"><FaYoutube size={32} color={"#65a30d"} /></a>
+      <a className="inline-block hover:scale-125 hover:opacity-75 transition" href="https://www.twitch.tv/meica05" target="_blank"><FaTwitch size={32} color={"#65a30d"} /></a>
+      <a className="inline-block hover:scale-125 hover:opacity-75 transition" href="https://www.tiktok.com/@meica05offcial" target="_blank"><FaTiktok size={32} color={"#65a30d"} /></a>
+      <a className="inline-block hover:scale-125 hover:opacity-75 transition" href="https://twitter.com/meica05" target="_blank"><FaTwitter size={32} color={"#65a30d"} /></a>
+      <a className="inline-block hover:scale-125 hover:opacity-75 transition" href="https://www.instagram.com/meica05/" target="_blank"><FaInstagram size={32} color={"#65a30d"} /></a>
     </div>
   )
 
